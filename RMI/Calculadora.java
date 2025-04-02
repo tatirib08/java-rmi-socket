0c0dc5a2ea1c4be2860681233af275593b987954
Tatiana Ribeiro <tatiroliveira08@gmail.com>
2025-04-02 13:41:34 -0300

    ajustes finais

--- RMI/Calculadora.java
+++
@@ -1,8 +0,0 @@
-import java.rmi.*;
-
-public interface Calculadora extends Remote {
-	int soma(int a, int b) throws RemoteException;
-	int subtracao(int a, int b) throws RemoteException;
-	int multiplicacao(int a, int b) throws RemoteException;
-	double divisao(int a, int b) throws RemoteException;
-}
